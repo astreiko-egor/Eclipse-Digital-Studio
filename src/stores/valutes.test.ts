207827268496cb54f3f76758730832f35cfe8330
astreiko-egor <astreiko.egor@gmail.com>
2023-02-02 22:30:40 +0300

    unit test store

---
+++ src/stores/valutes.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import { setActivePinia, createPinia } from 'pinia';
+import { valutesStore } from '@/stores/valutes';
+
+describe('Valutes Store', () => {
+  it('increments', async () => {
+    setActivePinia(createPinia());
+    const valutes = valutesStore();
+    expect(Object.keys(valutes.rates.today).length).toBe(0);
+    expect(Object.keys(valutes.rates.yesterday).length).toBe(0);
+    expect(Object.keys(valutes.valutes).length).toBe(0);
+    await valutes.addValutes();
+    expect(Object.keys(valutes.rates.today).length).not.toBe(0);
+    expect(Object.keys(valutes.rates.yesterday).length).not.toBe(0);
+    expect(Object.keys(valutes.valutes).length).not.toBe(0);
+  });
+});
